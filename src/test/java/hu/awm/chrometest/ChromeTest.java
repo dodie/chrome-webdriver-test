ed3f33a14f660668e47f4fee17cf1ab36ed741cf
David Csakvari <dodiehun@gmail.com>
2017-05-14 20:23:20 +0200

    Fix

--- src/test/java/hu/awm/chrometest/ChromeTest.java
+++ src/test/java/hu/awm/chrometest/ChromeTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeTest {
 
@@ -13,7 +14,7 @@ public class ChromeTest {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--no-sandbox");
-		WebDriver browser = new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver(options);
 		driver.get("http://www.google.com/");
 		WebElement body = driver.findElement(By.cssSelector("body"));
 		System.out.println(body.getText());
