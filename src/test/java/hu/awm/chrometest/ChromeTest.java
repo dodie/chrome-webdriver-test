8c81e1289c236347785bd3e806f136e8aebcdffd
David Csakvari <dodiehun@gmail.com>
2017-05-14 17:29:01 +0200

    init

---
+++ src/test/java/hu/awm/chrometest/ChromeTest.java
@@ -0,0 +1,20 @@
+package hu.awm.chrometest;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeTest {
+
+	@Test
+	public void testName() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com/");
+		WebElement body = driver.findElement(By.cssSelector("body"));
+		System.out.println(body.getText());
+		driver.quit();
+	}
+}
